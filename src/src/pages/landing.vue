2601d4fa4e06922662cecb8033114b2ba47afe3c
Barak Binaymin <barak.binyamin555@gmail.com>
2024-08-10 14:22:02 -0400

    Refactored css & made code pretty

--- src/src/pages/landing.vue
+++ src/src/pages/landing.vue
@@ -1,9 +1,9 @@
 <template>
+    <!-- Landing Page For Rockz -->
     <div class="landing">
-        <div class="padding">
-        </div>
+        <div class="padding"></div>
         <div class="logo">
-        <pre >                                      
+            <pre >                                      
                                                       __///\\__
                                                      ////////._\ 
   __________               __    __________         /\////////..\    
@@ -15,22 +15,22 @@
                                                    \     |#||__/      
                                                     \ __/#### /     
                                                      \#######/
-        </pre>
-        <div class="writer">
-            <div class="pointer"></div>
-            <div class="screen-cover"></div>
+            </pre>
+            <div class="shrinking-containter">
+                <div class="cursor-1"></div>
+                <div class="screen-cover"></div>
+            </div>
         </div>
-        </div>
-        <div class="container">
-            <div class="a">
-                <div class="c">
+        <div class="terminal-animation-prompt">
+            <div class="static-containter">
+                <div class="prompt-text">
                     {{prompt}}
                 </div>
-                <div class="b"></div>
+                <div class="cursor-2"></div>
             </div>
         </div>
 
-        <pre class="terminal-animation-done">
+        <pre class="terminal-animation-output">
 {{result}}
         </pre>
         <!-- In fututre releases put hidden hover ascii messages -->
@@ -88,32 +88,34 @@ export default{
     display     : block; 
     font-size   : var(--font-xsm);
     width       : min-content;
-    margin-left:  auto;
+    margin-left : auto;
     margin-right: auto;
     margin-top  : 60px;
 }
 .section{
-    font-family: monospace;
+    font-family   : monospace;
     text-transform: uppercase;
-    margin-top: 20px;
-    display   : grid;
-    color     : var(--font-color);
-    padding   : 10px;
-    text-align: center;
-    font-size : var(--font-sm);
-    border-bottom: 1px dashed lightgrey;
-    margin-left: 40px;
-    margin-right: 49px;
+    margin-top    : 20px;
+    display       : grid;
+    color         : var(--font-color);
+    padding       : 10px;
+    text-align    : center;
+    font-size     : var(--font-sm);
+    border-bottom : 1px dashed lightgrey;
+    margin-left   : 40px;
+    margin-right  : 49px;
 }
 .content{
     display   : grid;
     min-height: 300px;
 }
-@keyframes waiting{
-    0%{opacity: 0;height:0px} 
-    100%{opacity: 0;height:0px} 
+.terminal-animation-prompt{
+    display     : block;
+    width       : 427px;
+    margin-right: auto;
+    margin-left : auto;
 }
-.terminal-animation-done{
+.terminal-animation-output{
     display     : block;
     width       : 100%;
     font-size   : var(--font-xsm);
@@ -124,32 +126,66 @@ export default{
     margin-left:0px;
     padding    :0px;
 }
-.container{
-    display     : block;
-    width       : 427px;
-    margin-right: auto;
-    margin-left : auto;
-}
-.a{
-	position: relative;
-	height  : 25px;
-	display : block;
-    width   : max-content;
+.static-containter{
+	position   : relative;
+	height     : 25px;
+	display    : block;
+    width      : max-content;
     white-space: nowrap;
-    margin:0;
-    padding: 0px;
-    overflow: hidden;
-    font-size: var(--font-xsm);
+    margin     : 0;
+    padding    : 0px;
+    overflow   : hidden;
+    font-size  : var(--font-xsm);
 }
-.c{
-    height: 0px;
-    top : 0px;
-    left: 0px;
-	z-index: -10;
-    animation: showForASecond 2s linear;
-    overflow: hidden;
-    padding-left: 10px;
+.prompt-text{
+    height          : 0px;
+    top             : 0px;
+    left            : 0px;
+    animation       : showForASecond 2s linear;
+    overflow        : hidden;
+    padding-left    : 10px;
+    animation-delay : 5s;
+}
+.cursor-2{
+    position        : absolute;
+    top             : 0px;
+    width           : 100%;
+    min-height      : 15px;         
+    margin-left     : 100%;
+    background-color: var(--third-bg-color);
+    border-left     : .15em solid white;
+    animation       : 
+    typing2 1.5s steps(20, end),
+    blink-caret .15s step-end infinite;
     animation-delay: 5s;
+    opacity        : 1;
+}
+.screen-cover{
+    background-color: var(--third-bg-color);
+    height: 100%;
+}
+.shrinking-containter{
+    position: absolute;
+    display: block;
+    width: 100%;
+    height:  0;
+    bottom: 0;
+    left:0;
+    animation: 
+    shrink 5s steps(11, end);
+}
+.cursor-1{
+    background-color: var(--third-bg-color);
+    height:  1em;
+    min-height: 15px; 
+    border-left: 1px solid var(--third-bg-color);        
+    animation: 
+    typing2 .45s steps(20, end) 11,
+    blink-caret2 .15s step-end 30;
+}
+@keyframes waiting{
+    0%{opacity: 0;height:0px} 
+    100%{opacity: 0;height:0px} 
 }
 @keyframes showForASecond {
   0% {
@@ -159,27 +195,7 @@ export default{
     height: 25px;
   }
 }
-
-.b{
-    position: absolute;
-    top: 0px;
-	z-index: 5;
-    width: 100%;
-    min-height: 15px;         
-    margin-left: 100%;
-    background-color: var(--third-bg-color);
-    border-left: .15em solid white;
-    animation: 
-    typing2 1.5s steps(20, end),
-    blink-caret2 .15s step-end infinite;
-    animation-delay: 5s;
-    opacity: 1;
-}
-@keyframes show{
-  from{ opacity: 1}
-  to { opacity: 0}
-}
-@keyframes blink-caret2 {
+@keyframes blink-caret {
   from, to { border-color: transparent;}
   50% { border-color: white; }
 }
@@ -187,41 +203,11 @@ export default{
   from { margin-left: 0% }
   to { margin-left: 100%; }
 } 
-/*  
-<div container>
-    blinker
-    reset of fill - not automated
-<div>   
-*/
-
-.screen-cover{
-    background-color: var(--third-bg-color);
-    height: 100%;
-}
-.writer{
-    position: absolute;
-    display: block;
-    width: 100%;
-    height:  0;
-    bottom: 0;
-    left:0;
-    animation: 
-    show 5s steps(11, end);
-}
-.pointer{
-    background-color: var(--third-bg-color);
-    height:  1em;
-    min-height: 15px; 
-    border-left: 1px solid var(--third-bg-color);        
-    animation: 
-    typing2 .45s steps(20, end) 11,
-    blink-caret3 .15s step-end 30;
-}
-@keyframes show {
+@keyframes shrink {
   from { height: calc(100% - 1em)}
   to { height: 0; }
 } 
-@keyframes blink-caret3 {
+@keyframes blink-caret2 {
   from, to { border-color: transparent;}
   50% { border-color: white; }
 }
